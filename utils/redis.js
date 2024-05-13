6925e67593b1236f6c3d687f2363371d137a145b
KelvinoKing <kelvinoking@outlook.com>
2024-05-13 10:38:38 +0300

    Utils

--- utils/redis.js
+++ utils/redis.js
@@ -14,37 +14,33 @@
  * After the class definition, create and export an instance of RedisClient called redisClient.
  */
 
-
 const redis = require('redis');
 const { promisify } = require('util');
 
-
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (error) => {
-        console.log(`Redis client not connected to the server: ${error.message}`);
-        });
-    }
-    
-    isAlive() {
-        return this.client.connected;
-    }
-    
-    async get(key) {
-        const asyncGet = promisify(this.client.get).bind(this.client);
-        const value = await asyncGet(key);
-        return value;
-    }
-    
-    async set(key, value, duration) {
-        this.client.set(key, value);
-        this.client.expire(key, duration);
-    }
-    
-    async del(key) {
-        this.client.del(key);
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
+
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
