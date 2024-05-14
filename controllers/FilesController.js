697386ea2f71de3c8dde6e73c1af35671aa32ab9
KelvinoKing <kelvinoking@outlook.com>
2024-05-14 09:54:45 +0300

    Get and list file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -96,6 +96,70 @@ class FilesController {
     await dbClient.client.db().collection('files').insertOne(newFile);
     return res.status(201).send(newFile);
   }
+
+  /**
+   * GET /files/:id => FilesController.getShow
+   * GET /files => FilesController.getIndex
+   * In the file controllers/FilesController.js, add the 2 new endpoints:
+   * GET /files/:id should retrieve the file document based on the ID:
+   * Retrieve the user based on the token:
+   * If not found, return an error Unauthorized with a status code 401
+   * If no file document is linked to the user and the ID passed as parameter,
+   * return an error Not found with a status code 404
+   * Otherwise, return the file document
+   * GET /files should retrieve all users file documents for a specific parentId
+   * and with pagination:
+   *
+   * Retrieve the user based on the token:
+   * If not found, return an error Unauthorized with a status code 401
+   * Based on the query parameters parentId and page, return the list of file document
+   * parentId:
+   * No validation of parentId needed - if the parentId is not linked to any user folder,
+   * returns an empty list
+   * By default, parentId is equal to 0 = the root
+   * Pagination:
+   * Each page should be 20 items max
+   * page query parameter starts at 0 for the first page. If equals to 1,
+   * it means it’s the second page (form the 20th to the 40th), etc…
+   * Pagination can be done directly by the aggregate of MongoDB} req
+   */
+  static async getShow(req, res) {
+    const token = req.header('X-Token');
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const { id } = req.params;
+    const file = await dbClient.client.db().collection('files').findOne({ _id: ObjectId(id), userId: ObjectId(userId) });
+
+    if (!file) return res.status(404).send({ error: 'Not found' });
+    return res.status(200).send(file);
+  }
+
+  static async getIndex(req, res) {
+    const token = req.header('X-Token');
+    if (!token) return res.status(401).send({ error: 'Unauthorized' });
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+
+    const { parentId } = req.query;
+    const page = req.query.page || 0;
+    const limit = 20;
+
+    const files = await dbClient.client.db().collection('files')
+      .find({ parentId: parentId || '0', userId: ObjectId(userId) })
+      .limit(limit)
+      .skip(page * limit)
+      .toArray();
+
+    return res.status(200).send(files);
+  }
 }
 
 export default FilesController;
